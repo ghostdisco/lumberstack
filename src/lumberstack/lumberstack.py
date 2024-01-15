caacdf4ffb62173ef11ba8341dcb1b247a9ec014
ghostdisco <gith@ghostdisco.me>
2024-01-15 01:51:09 -0500

    discord support!

--- src/lumberstack/lumberstack.py
+++ src/lumberstack/lumberstack.py
@@ -11,6 +11,24 @@ from logging import Logger, Handler
 # DEBUG = 10
 # NOTSET = 0
 
+class _Dummy_Logger_:
+  def __init__(self, name: str = 'dummy') -> None:
+    self.base = logging.getLogger(name=name)
+    self.base.debug = _Dummy_Logger_._dummy_log_
+    self.base.info = _Dummy_Logger_._dummy_log_
+    self.base.warn = _Dummy_Logger_._dummy_log_
+    self.base.warning = _Dummy_Logger_._dummy_log_
+    self.base.error = _Dummy_Logger_._dummy_log_
+    self.base.critical = _Dummy_Logger_._dummy_log_
+
+  @staticmethod
+  def _dummy_log_(arg0: 'typing.any' = None, arg1: 'typing.any' = None, 
+                  arg2: 'typing.any' = None, arg3: 'typing.any' = None, 
+                  arg4: 'typing.any' = None, arg5: 'typing.any' = None):
+    pass
+
+lumberstack_formatting: logging.Formatter
+
 class Lumberstack:
   last_msg: str = None
   history: list[str] = []
@@ -28,41 +46,59 @@ class Lumberstack:
 
   # run once from __main__
   @staticmethod
-  def global_init(timezone: time.struct_time = time.localtime, log_filename: str = None, log_level: int = logging.INFO, format: str = '%(asctime)s %(name)s %(levelname)s: %(message)s', console_output: bool = True, custom_handlers: list[Handler] = None):
+  def global_init(timezone: time.struct_time = time.localtime, log_filename: str = None, log_level: int = logging.INFO, format: str = '%(asctime)s %(name)s %(levelname)s: %(message)s', console_output: bool = True, custom_handlers: list[Handler] = None, mute_errors_from_lumberstack: bool = False):
 
     # initialize global instance of logger module
     logging.Formatter.converter = timezone
     logging.basicConfig(filename=log_filename, level=log_level, format=format)
     root_logger = logging.getLogger()
-    formatting = logging.Formatter(fmt=format)
+    global lumberstack_formatting
+    lumberstack_formatting = logging.Formatter(fmt=format)
 
     # remove default handlers
     for h in root_logger.handlers:
       root_logger.removeHandler(hdlr=h)
-    
+
     # set file handler
     if log_filename:
       fh = logging.FileHandler(filename=log_filename)
-      fh.setFormatter(formatting)
+      fh.setFormatter(lumberstack_formatting)
       root_logger.addHandler(hdlr=fh)
-      if log_level < logging.INFO:
+      if not mute_errors_from_lumberstack and log_level < logging.INFO:
         Lumberstack(name='lumberstack').debug(f'File Handler Added: {log_filename}')
-    
+
     # set console handler
     if console_output:
       ch = logging.StreamHandler(stream=sys.stdout)
-      ch.setFormatter(formatting)
+      ch.setFormatter(lumberstack_formatting)
       root_logger.addHandler(hdlr=ch)
-      if log_level < logging.INFO:
+      if not mute_errors_from_lumberstack and log_level < logging.INFO:
         Lumberstack(name='lumberstack').debug(f'Console Handler Added: STDOUT')
     
     # add custom handlers
     if custom_handlers:
       for h in custom_handlers:
-        h.setFormatter(formatting)
+        h.setFormatter(lumberstack_formatting)
         root_logger.addHandler(hdlr=h)
-        if log_level < logging.INFO:
-          Lumberstack(name='lumberstack').debug(f'Custom Handler Added: {h.get_name()}')
+        if not mute_errors_from_lumberstack and log_level < logging.INFO:
+          Lumberstack(name='lumberstack').debug(f'Handler Added: {type(h).__name__}{" - " + h.name if h.name else ""}')
+
+    # mute me if you desire
+    if mute_errors_from_lumberstack:
+      Lumberstack.mute_library_logging(libraries='lumberstack')
+
+  # add a handler
+  @staticmethod
+  def add_handlers(handlers: Handler | list[Handler]) -> None:
+    if isinstance(handlers, Handler):
+      handlers = [handlers]
+
+    global lumberstack_formatting
+    for h in handlers:
+      h.setFormatter(lumberstack_formatting)
+      logging.getLogger().addHandler(hdlr=h)
+      Lumberstack(name='lumberstack').debug(f'Handler Added: {type(h).__name__}{" - " + h.name if h.name else ""}')
+
 
   # retrieve logger by name
   @staticmethod
@@ -84,23 +120,26 @@ class Lumberstack:
     for l in libraries:
       logger = Lumberstack.get_logger(name=l)
       if log_level > logging.DEBUG:
-        logger.debug = Lumberstack._dummy_log_
+        logger.debug = _Dummy_Logger_._dummy_log_
       if log_level > logging.INFO:
-        logger.info = Lumberstack._dummy_log_
+        logger.info = _Dummy_Logger_._dummy_log_
       if log_level > logging.WARN:
-        logger.warn = Lumberstack._dummy_log_
-        logger.warning = Lumberstack._dummy_log_
+        logger.warn = _Dummy_Logger_._dummy_log_
+        logger.warning = _Dummy_Logger_._dummy_log_
       if log_level > logging.ERROR:
-        logger.error = Lumberstack._dummy_log_
+        logger.error = _Dummy_Logger_._dummy_log_
       if log_level > logging.FATAL:
-        logger.fatal = Lumberstack._dummy_log_
-        logger.critical = Lumberstack._dummy_log_
+        logger.fatal = _Dummy_Logger_._dummy_log_
+        logger.critical = _Dummy_Logger_._dummy_log_
 
+  # forcibly mute a logger
   @staticmethod
-  def _dummy_log_(arg0: 'typing.any' = None, arg1: 'typing.any' = None, 
-                  arg2: 'typing.any' = None, arg3: 'typing.any' = None, 
-                  arg4: 'typing.any' = None, arg5: 'typing.any' = None):
-    pass
+  def mute_library_logging(libraries: str | list[str]):
+    Lumberstack.force_update_library_levels(libraries=[libraries] if isinstance(libraries, str) else libraries, log_level=100)
+
+  @property
+  def _my_logger_(self):
+    return Lumberstack(name='lumberstack')
 
   def critical(self, msg):
     self._log_(msg=msg, level=logging.CRITICAL)
@@ -124,7 +163,10 @@ class Lumberstack:
     try:
       msg = str(msg)
     except Exception as error:
-      raise Exception(error)
+      level_name = logging.getLevelName(level=level)
+      self._my_logger_.error(f'failed to log {level_name} message, could not cast to string value... see following error for details')
+      self._my_logger_.error(str(error))
+      return
 
     if self.capitalize_messages:
       msg = f'{msg[0].upper()}{msg[1:]}'
--- todo.md
+++ todo.md
@@ -1,13 +1,3 @@
-[ ] add 'use local if debugging else use gmt' option to global init?
-[ ] custom exceptions
-    [ ] if cannot convert msg to string
-[ ] granular handler control
-    [ ] ability to set log file and console log_level on global_init
-    [ ] add custom handlers built off Handler class
-        [ ] create DiscordHandler with basic options
-            * webhook
-            * timezone (should default to global if not provided)
-            * loglevel (^^^)
-            * formatting (^^^)
-            * ... anything else?
-        [ ] add Teams the same way
+[ ] custom handlers
+    [ ] RabbitMQ
+    [ ] Teams

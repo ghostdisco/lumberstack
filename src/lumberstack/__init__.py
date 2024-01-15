0f375d1f30228fda25d3a64b455194ee3533f10b
ghostdisco <gith@ghostdisco.me>
2024-01-15 17:04:24 -0500

    improve custom handlers

--- src/lumberstack/__init__.py
+++ src/lumberstack/__init__.py
@@ -3,4 +3,5 @@ Lumberstack - A simple abstraction of the logging package
 """
 
 from .lumberstack import *
-from .custom_handlers import *
+from .custom_handler import CustomHandler
+from .discord import DiscordHandler
